3822bc4e99ddb72a3bfc987781361bb291549b44
kimbongjin <zzaiba17@naver.com>
2021-08-15 14:08:51 +0900

    init

---
+++ app/src/main/java/com/taskagile/TaskAgileApplication.java
@@ -0,0 +1,16 @@
+package com.taskagile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class TaskAgileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAgileApplication.class, args);
+	}
+
+}
